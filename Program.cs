983e3bc39f153ec5118fabce2b187b42c94f06cb
nino pitskhelauri <pitskhelauri.nini@gmail.com>
2024-09-25 23:10:19 +0200

    Initial commit with project files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.WriteLine("Nini Pitskhelauri");
+Console.WriteLine("Imedia Gamova");
+String userInput = Console.ReadLine();
+Console.WriteLine("Nini Pitskhelauri" + userInput);
+Console.ReadKey();
+
